5e2ba403e221164c0876628eaeefa2104d878230
Excoriate <atr.engineer@outlook.com>
2016-03-18 20:14:39 -0300

    Version de clases.

---
+++ UN1Taller2clase/blackend/AppConfigManager/ConfigReader.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackend.AppConfigManager
+{
+    /// <summary>
+    /// Doc. 
+    /// </summary>
+    public class ConfigReader
+    {
+
+        /// <summary>
+        /// Mi comentario, con 3 /// seguidos.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string ObtenerValorPorIdentificador(string key)
+        {
+            string valor = default(string);
+
+            //if (key == "")
+            //{
+                
+            //}
+
+            //if (!string.IsNullOrWhiteSpace(key))
+            //{
+            //    var asdsad = "";
+            //    var asdasdasa = "  ";
+            //}
+
+            if (!string.IsNullOrEmpty(key))
+            {
+                //aca, recien me preocupo de obtener el valor desde el config.
+                //usando la clase ConfigurationSettings que es, como lo dij
+                //en la primera clase el profesor, parte del CLB. 
+                var miConfigurador = ConfigurationSettings.AppSettings[key];
+
+                if (!object.ReferenceEquals(miConfigurador, null))
+                {
+                    valor = miConfigurador.ToString();
+
+                }
+
+                //if (miConfigurador != null)
+                //{
+                    
+                //}
+
+                //if (!miConfigurador.Equals(default(object)))
+                //{
+                    
+                //}
+
+
+            }
+
+
+            return valor;  
+        }
+
+        public int ObtenerLargo(string value)
+        {
+            var largo = default(int);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                largo = value.Length;
+            }
+
+            return largo;
+
+            //return value.Length;
+            //return !string.IsNullOrEmpty(value)
+            //    ? value.Length : default(int);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ UN1Taller2clase/frontend/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blackend.AppConfigManager;
+
+namespace frontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configReader = new ConfigReader(); 
+
+            //Obtener desde el config, especificamente, desde el backend. 
+            string miNombreDesdeElConfig = configReader.ObtenerValorPorIdentificador("minombre");
+
+            string largoDelNombre = configReader.ObtenerLargo(miNombreDesdeElConfig).ToString();
+
+            //dar formato a mensaje con el metodo format de la clase string.
+            string msjBievenida = string.Format("Bienvenido pelagato : {0}", miNombreDesdeElConfig);
+            string msjDelLargo = string.Format("\nel largo de tu nombre es: {0}", largoDelNombre);
+            //string algoconcatenado = "hola" + "pelagato";
+            string algoconcatenado = string.Concat("tustring1", "tustring2");
+
+            //Generando concatenacion con StringBuilder.
+            //StringBuilder objBuilder = new StringBuilder();  //genero instancia. 
+            //objBuilder.Append("tustring1");
+            //objBuilder.Append("tustring2");
+            //objBuilder.Append("tustring3");
+            //objBuilder.Append("tustring4");
+            //objBuilder.Append("tustring5");
+            //objBuilder.ToString();
+
+
+            //como inicializo.
+            //string algo = string.Empty;
+            //string algo3 = default(string);
+
+
+            Console.WriteLine(msjBievenida);
+            Console.WriteLine(msjDelLargo);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
