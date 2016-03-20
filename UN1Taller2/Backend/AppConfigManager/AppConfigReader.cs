52b7c3364d599b216c2f60303d7b1c2d13faf603
Excoriate <atr.engineer@outlook.com>
2016-03-20 20:55:28 -0300

    Taller nro 2 , resuelto por profesor.  Evaluacion lunes 21

---
+++ UN1Taller2/Backend/AppConfigManager/AppConfigReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using System.Globalization;
+
+namespace Backend.AppConfigManager
+{
+    public class AppConfigReader
+    {
+
+        public string GetValueFromKeyInAppConfig(string key)
+        {
+            var value = string.Empty;
+
+            if (!string.IsNullOrEmpty(key))
+            {
+                var auxValueObject = ConfigurationManager.AppSettings[key];
+
+                if (!object.ReferenceEquals(auxValueObject, null))
+                {
+                    value = auxValueObject.ToString(CultureInfo.InvariantCulture);
+                    //Pueden usar tambien el ToString(); sin la necesidad de usar la sobrecarga
+                    //que use en la línea 25.
+                }
+            }
+
+            return value;
+
+        }
+
+
+
+
+    }
+}
