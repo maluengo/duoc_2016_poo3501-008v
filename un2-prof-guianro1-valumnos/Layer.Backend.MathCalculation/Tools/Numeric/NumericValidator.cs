c733eec52c936bb5881144c560ba33255d5c1968
Excoriate <atr.engineer@outlook.com>
2016-04-16 16:47:49 -0300

    Base project for Guia nro 1, Unidad 2.

---
+++ un2-prof-guianro1-valumnos/Layer.Backend.MathCalculation/Tools/Numeric/NumericValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Backend.MathCalculation.Tools.Numeric
+{
+    public class NumericValidator
+    {
+        /// <summary>
+        /// ATR: Valida, mediante un boolean, si el número es efectivamente convertible a INT DE 32 bits. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsStringNumericConvertible(string value)
+        {
+            var isOk = false;
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                var auxIntParse = default(int);
+                isOk = int.TryParse(value, out auxIntParse);
+
+            }
+
+            return isOk;
+        }
+    }
+}
