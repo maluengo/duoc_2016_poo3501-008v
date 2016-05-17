d4f075d9b79c080d52e7fceeecdb1790c1bee4a8
Excoriate <atr.engineer@outlook.com>
2016-05-16 21:07:51 -0400

    Unit testing negative assert finished

--- un2_wpf_ejercicio1/layer.backend.fakeRepository/Reader/Files/FileReader.cs
+++ un2_wpf_ejercicio1/layer.backend.fakeRepository/Reader/Files/FileReader.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using layer.backend.fakeRepository.Contracts;
 
 namespace layer.backend.fakeRepository.Reader.Files
@@ -16,9 +18,54 @@ namespace layer.backend.fakeRepository.Reader.Files
         //    throw new System.NotImplementedException();
         //}
 
+        public DirectoryInfo LoadDirectory(string path)
+        {
+            var objDirectory = default(DirectoryInfo);
+
+            if (IsPathValid(path))
+            {
+                objDirectory = new DirectoryInfo(path);
+            }
+
+            return objDirectory;
+        }
+
+        public bool IsPathValid(string path)
+        {
+            var isValid = false;
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                try
+                {
+                    isValid = File.Exists(path);
+                }
+                catch (Exception)
+                {
+                    
+                }
+
+            }    
+
+            return isValid;
+        }
+
+        
+
         public IEnumerable<FileInfo> GetAllFilesFromPath(string path)
         {
-            throw new System.NotImplementedException();
+            List<FileInfo> collectionOfFiles = default(List<FileInfo>);
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (IsPathValid(path))
+                {
+                    collectionOfFiles = this.LoadDirectory(path).GetFiles().ToList();
+                }
+            }
+                
+            return collectionOfFiles;
+            
         }
 
         public IEnumerable<FileInfo> GetOnlyLogFiles(string path)
--- un2_wpf_ejercicio1/layer.tdd.backend/Reader/Files/FileReaderTests.cs
+++ un2_wpf_ejercicio1/layer.tdd.backend/Reader/Files/FileReaderTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using layer.backend.fakeRepository.Contracts;
+using layer.backend.fakeRepository.Reader.Files;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace layer.tdd.backend.Reader.Files
 {
@@ -8,7 +10,21 @@ namespace layer.tdd.backend.Reader.Files
         [TestMethod()]
         public void GetAllFilesFromPathTest()
         {
-            Assert.Fail();
+            IFileReaderable objTest = new FileReader();
+
+            var test1 = objTest.GetAllFilesFromPath("invalido");
+            var test2 = objTest.GetAllFilesFromPath(null);
+            var test3 = objTest.GetAllFilesFromPath(string.Empty);
+
+            Assert.IsNull(test1);
+            Assert.IsNull(test2);
+            Assert.IsNull(test3);
+
+            var test4 = objTest.GetAllFilesFromPath(@"D:\Dropbox\SOURCE-GitHub\poo3501\logs");
+
+
+
+
         }
 
         [TestMethod()]
